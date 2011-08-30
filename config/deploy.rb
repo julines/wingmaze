b9d8fffcf17df12cdca2ac44f16d7eb87c10677c
Julines Liong <julines@gmail.com>
2011-08-30 12:29:29 +1000

    fixing deployment script

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,45 +1,37 @@
 require 'bundler/capistrano'
-# set :application, "set your application name here"
-# set :repository,  "set your repository location here"
-# 
-# set :scm, :subversion
-# # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-# 
-# role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-# role :app, "your app-server here"                          # This may be the same as your `Web` server
-# role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-# role :db,  "your slave db-server here"
 
 default_run_options[:pty] = false
 ssh_options[:forward_agent] = true
 set :use_sudo, false
 set :user, 'julines'
 
-# set :domain, 'wingmaze'
-
-# set :domain, 'julinesliong.com'
-set :domain, 'moroni.dreamhost.com'
 set :application, "wingmaze"
-set :repository,  "git@github.com:julines/wingmaze.git"
+set :repository, "git@github.com:julines/wingmaze.git"
 set :scm, :git
-set :scm_passphrase, "gideon277"
-set :stages, %w(development uat production)
-# set :branch, 'master'
+set :branch, 'master'
 set :git_shallow_clone, 1
 set :deploy_via, :remote_cache
 set :copy_compression, :bz2
-set :rails_env, 'production'
+set :rails_env, :development
 set :deploy_to, "/home/julines/#{application}"
 
 role :web, "#{application}"                          # Your HTTP server, Apache/etc
 role :app, "#{application}"                          # This may be the same as your `Web` server
 role :db,  "#{application}", :primary => true        # This is where Rails migrations will run
 
-task :development do
+task :staging do
   role :app, "#{domain}"
   role :web, "#{domain}"
   role :db,  "#{domain}", :primary => true
   set :stage, :development
+  set :branch, "master"
+end
+
+task :uat do
+  role :web, "#{application}"
+  role :app, "#{application}"
+  role :db, "#{application}", :primary => true
+  set :stage, :uat
   set :branch, "master"
 end
 
